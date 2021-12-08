95a1a587947320c460055a09fc22918a34badcd9
yusuf emad <yusufy2004@gmail.com>
2021-12-08 23:08:31 +0200

    removed -ve angles

--- controllers/rcj_soccer_team_blue/functions.py
+++ controllers/rcj_soccer_team_blue/functions.py
@@ -3,4 +3,5 @@ import math
 def get_angle(directions):
     LR = directions[0]
     FB = directions[2]
-    return -math.atan2(LR, FB)*180/math.pi
+    angle = -math.atan2(LR, FB)*180/math.pi
+    return angle if angle > 0 else 360 + angle
